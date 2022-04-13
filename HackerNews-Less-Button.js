4479d3dbe8f71cac8223c4e6a42254a14712a897
itsHel <51739304+itsHel@users.noreply.github.com>
2022-04-13 12:41:16 +0200

    Update HackerNews-Less-Button.js

--- HackerNews-Less-Button.js
+++ HackerNews-Less-Button.js
@@ -1,27 +1,47 @@
 // ==UserScript==
 // @name         HackerNews Less Button
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      1.0
 // @description  try to take over the world!
-// @author       You
+// @author       Hel
 // @match        *news.ycombinator.com/*
 // ==/UserScript==
 
 (function() {
-    'use strict';
+    try{
+        let pageNumber = location.search.match(/p=(\d+)/);
 
-    let link = document.querySelector(".morelink");
+        if(!pageNumber || pageNumber[1] == 1){
+            return;
+        } else {
+            pageNumber = pageNumber[1];
+        }
 
-    let newHref = link.href.replace(/\d+/, function(val){return val - 2});
+        let link = document.querySelector(".morelink");
 
-    let newLink = link.cloneNode();
+        if(link){
+            let newHref = link.href.replace(/\d+/, pageNumber - 1);
+            let newLink = link.cloneNode();
 
-    newLink.href = newHref;
-    newLink.textContent = "Less";
+            newLink.href = newHref;
+            newLink.textContent = "Less";
 
-    link.parentNode.style.display = "flex";
-    link.parentNode.style.flexDirection = "column";
-    link.parentNode.style.gap = "8px";
+            link.parentNode.style.display = "flex";
+            newLink.style.marginLeft = "auto";
+            newLink.style.marginright = "3rem";
 
-    link.insertAdjacentElement("beforebegin", newLink);
+            link.insertAdjacentElement("afterend", newLink);
+        } else {
+            let parent = document.querySelector("table.itemlist tbody");
+
+            let newHtml = `
+                <tr class="morespace" style="height:10px"></tr>
+                <tr>
+                    <td colspan="2"></td>
+                    <td class="title"><a href="${location.href.replace(/p=\d+/, "p=" + (pageNumber - 1) + "")}" class="morelink" rel="next">Less</a></td>
+                </tr>`;
+
+            parent.insertAdjacentHTML("beforeend", newHtml);
+        }
+    }catch(e){}
 })();
